32f9179039cffb52d3d72be597cd0ccf66a49c12
glima <glima@vitalitecnologia.com.br>
2017-01-02 15:56:41 -0200

    [DEV] refactoring

--- ApiConsumer/Program.cs
+++ ApiConsumer/Program.cs
@@ -1,11 +1,5 @@
 ﻿using ApiConsumer.Services.Api;
-using ApiConsumer.VM;
 using ApiConsumer.VM.Out;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ApiConsumer
 {
